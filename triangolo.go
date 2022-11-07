3d0e1929561df54b180aa9b8625bf3ce00a36acd
LucaDalMas <luca.dalmas@icloud.com>
2022-11-07 13:26:11 +0100

    Adjust Andrea-Capra.go

--- triangolo.go
+++ triangolo.go
@@ -5,9 +5,9 @@ import "fmt"
 func main() {
 	var n int
 	fmt.Scan(&n)
-	for i := 0; i < (n*2)-1; i++ {
-		for j := 0; j < (n*2)-1; j++ {
-			if (i == j) || ( i==0 && j<n ) || (i==n*2-2 && j>=n) || (j == n-1) {
+	for i := 0; i < (n*2); i++ {
+		for j := 0; j < (n * 2)-1; j++ {
+			if (i == j && i < n) || (i == j+1 && i > n) || (i == 0 && j < n) || (i == n*2-1 && j >= n) || (j == n-1) {
 				fmt.Print("*")
 			} else {
 				fmt.Print(" ")
